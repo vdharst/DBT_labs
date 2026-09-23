971fe2864f5eb75246b96d69f6bfa8a54256597c
Diederik van der Harst <diederik.vanderharst@ddbm.com>
2026-09-23 19:07:25 +0000

    Commit of exemplar: creating fct_orders, refactoring of dim_customer plus stg_stripe_payments

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,6 +1,30 @@
--- --Create a fct_orders.sql (not stg_orders) model with the following fields.  Place this in the marts/financedirectory.
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+),
 
---     order_id
---     customer_id
---     amount (hint: this has to come from payments)
+payments as (
+    select * from {{ ref ('stg_stripe__payments') }}
+),
 
+order_payments as (
+    select
+        order_id,
+        sum (case when payment_status = 'success' then payment_amount end) as amount
+
+    from payments
+    group by 1
+),
+
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
+)
+
+select * from final
--- models/staging/jaffle_shop/stg_stripe__payments.sql
+++ models/staging/jaffle_shop/stg_stripe__payments.sql
@@ -1,4 +1,26 @@
-select
-    *
-from raw.stripe.payment
- 
+
+
+with 
+
+source as (
+
+    select * from raw.stripe.payment
+
+),
+
+renamed as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status as payment_status,
+        amount as payment_amount,
+        created as payment_created,
+        _batched_at
+
+    from source
+
+)
+
+select * from renamed
