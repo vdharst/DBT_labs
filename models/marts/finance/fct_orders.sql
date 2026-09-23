f98480febc2236760a5103e723682e8737affac3
Diederik van der Harst <diederik.vanderharst@ddbm.com>
2026-09-23 18:11:14 +0000

    Added fact table

---
+++ models/marts/finance/fct_orders.sql
@@ -0,0 +1,6 @@
+-- --Create a fct_orders.sql (not stg_orders) model with the following fields.  Place this in the marts/financedirectory.
+
+--     order_id
+--     customer_id
+--     amount (hint: this has to come from payments)
+
---
+++ models/staging/jaffle_shop/stg_stripe__payments.sql
@@ -0,0 +1,4 @@
+select
+    *
+from raw.stripe.payment
+ 
